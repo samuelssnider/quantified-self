d9218d2d9a0f255a3744f9eea96016d3294a6932
samuelssnider <sam_redins@yahoo.com>
2017-11-30 10:47:06 -0700

    it was literally all my tags

--- main.bundle.js
+++ main.bundle.js
@@ -71,31 +71,20 @@
 /* 2 */
 /***/ (function(module, exports) {
 
-	"use strict";
+	'use strict';
 
 	var API = "http://localhost:3000";
 
 	$(document).ready(function () {
 
-	  var handleError = function handleError() {
-	    alert("Something went wrong, please try again");
-	  };
-
 	  $.ajax({
-	    url: API + '/api/v1/foods',
+	    url: API + '/api/v1/foods/',
 	    method: 'GET'
 	  }).then(function (data) {
-	    for (var i = 0; data.length; i++) {
-	      $('.food-table').append("<tr><tb>" + data.name + "</tb><tb>" + data.calories + "</tb></tr>");
+	    for (var i = 0; i < data.length; i++) {
+	      $("table").append('<tr><tb>' + data[i]['name'] + '</tb><tb>' + data[i]['calories'] + '</tb></tr>');
 	    }
 	  });
-	  // .done(function(data) {
-	  //   for(var i = 0; data.length; i ++){
-	  //     $('.food-table').append(`<tr><tb>${data.name}</tb><tb>${data.calories}</tb></tr>`);
-	  //   }
-	  // }).fail(function(){
-	  //   handleError();
-	  // });
 	});
 
 /***/ })
